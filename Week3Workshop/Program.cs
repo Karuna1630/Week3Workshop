00686dcd5f01e5b097cc01f08bbed704871b0ea6
Karuna1630 <karunagiri481@gmail.com>
2025-11-10 11:52:20 +0545

    Create method in Operators class and did simple arithmetic operators

--- Week3Workshop/Program.cs
+++ Week3Workshop/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Operators operators = new Operators();
+            Console.WriteLine("Addition: " + operators.Add(5, 3));
+            Console.WriteLine("Subtraction: " + operators.Subtract(5, 3));
+            Console.WriteLine("Multiplication: " + operators.Multiply(5, 3));
+            Console.WriteLine("Division: " + operators.Divide(6, 3));
+
         }
     }
+
+    //public class Operators
+    //{
+    //   public int Add(int a, int b)
+    //    {
+    //        return a + b;
+    //    }
+
+    //    public int Subtract(int a, int b)
+    //    {
+    //        return a - b;
+    //    }
+    //    public int Multiply(int a, int b)
+    //    {
+    //        return a * b;
+    //    }
+    //    public int Divide(int a, int b)
+    //    {
+    //        return a / b;
+    //    }
+    //}
 }
