bb366f7f51cc8b62900feafeed98b1e006d59f6a
Karuna1630 <karunagiri481@gmail.com>
2025-11-10 21:56:03 +0545

    Task 3: If else and Switch

--- Week3Workshop/Program.cs
+++ Week3Workshop/Program.cs
@@ -5,19 +5,36 @@
         static void Main(string[] args)
         {
 
-            Operators operators = new Operators();
-            NullOperations NullOperators = new NullOperations();
+            //Operators operators = new Operators();
+            //NullOperations NullOperators = new NullOperations();
 
-            //calling arithmetic operation methods
-            Console.WriteLine("Addition: " + operators.Add(5, 3));
-            Console.WriteLine("Subtraction: " + operators.Subtract(5, 3));
-            Console.WriteLine("Multiplication: " + operators.Multiply(5, 3));
-            Console.WriteLine("Division: " + operators.Divide(6, 3));
-            Console.WriteLine("Find Odd or Even (5): " + operators.FindOddEven(5));
+            ////calling arithmetic operation methods
+            //Console.WriteLine("Addition: " + operators.Add(5, 3));
+            //Console.WriteLine("Subtraction: " + operators.Subtract(5, 3));
+            //Console.WriteLine("Multiplication: " + operators.Multiply(5, 3));
+            //Console.WriteLine("Division: " + operators.Divide(6, 3));
+            //Console.WriteLine("Find Odd or Even (5): " + operators.FindOddEven(5));
 
-            //calling the method to perform null checks
-            NullOperators.PerformNullChecks();
+            ////calling the method to perform null checks
+            //NullOperators.PerformNullChecks();
 
+
+            // task3: If else and Switch
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else
+            {
+                Console.WriteLine("Adult");
+            }
 
 
         }
