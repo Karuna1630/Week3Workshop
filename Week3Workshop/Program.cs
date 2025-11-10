031871cb1be0ad650ed381b8b1312b81130d38db
Karuna1630 <karunagiri481@gmail.com>
2025-11-10 22:08:29 +0545

    Task 4 : If else and Switch Statement

--- Week3Workshop/Program.cs
+++ Week3Workshop/Program.cs
@@ -19,45 +19,81 @@
             //NullOperators.PerformNullChecks();
 
 
-            // task3: If else and Switch
-            Console.WriteLine("Enter your age: ");
-            int age = int.Parse(Console.ReadLine());
+            //// task3: If else and Switch
+            //Console.WriteLine("Enter your age: ");
+            //int age = int.Parse(Console.ReadLine());
 
-            if (age < 13)
-            {
-                Console.WriteLine("Child");
-            }
-            else if (age >= 13 && age <= 19)
-            {
-                Console.WriteLine("Teenager");
-            }
-            else
-            {
-                Console.WriteLine("Adult");
-            }
+            //if (age < 13)
+            //{
+            //    Console.WriteLine("Child");
+            //}
+            //else if (age >= 13 && age <= 19)
+            //{
+            //    Console.WriteLine("Teenager");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Adult");
+            //}
 
 
+            // Task 4: If else and Switch 
+            Console.WriteLine("Enter a number (1-7) for day of the week: ");
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            //Using switch statement
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+
+
+
+
+            }
         }
+
+        //public class Operators
+        //{
+        //   public int Add(int a, int b)
+        //    {
+        //        return a + b;
+        //    }
+
+        //    public int Subtract(int a, int b)
+        //    {
+        //        return a - b;
+        //    }
+        //    public int Multiply(int a, int b)
+        //    {
+        //        return a * b;
+        //    }
+        //    public int Divide(int a, int b)
+        //    {
+        //        return a / b;
+        //    }
+        //}
     }
-
-    //public class Operators
-    //{
-    //   public int Add(int a, int b)
-    //    {
-    //        return a + b;
-    //    }
-
-    //    public int Subtract(int a, int b)
-    //    {
-    //        return a - b;
-    //    }
-    //    public int Multiply(int a, int b)
-    //    {
-    //        return a * b;
-    //    }
-    //    public int Divide(int a, int b)
-    //    {
-    //        return a / b;
-    //    }
-    //}
 }
